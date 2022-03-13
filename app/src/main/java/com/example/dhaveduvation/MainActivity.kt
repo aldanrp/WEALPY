9f3608d79976439554f1843a30052bce08a39da3
Aldan <rallfirdan@gmail.com>
2022-03-13 13:06:09 +0700

    first commit

---
+++ app/src/main/java/com/example/dhaveduvation/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.dhaveduvation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private  lateinit var tvClickSign : TextView
+    private  lateinit var tvEmail : TextView
+    private  lateinit var tvPassword : TextView
+    private  lateinit var btnLogin : Button
+
+    companion object {
+       var AuthUsername = "EXTRA_NAME"
+        var Authpassword = "EXTRA_PASS"
+    }
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == SignUpActivity.RESULT_CODE && result.data != null) {
+            val selectedEmail = result.data?.getStringExtra(SignUpActivity.EXTRA_Email)
+            val selectedPass = result.data?.getStringExtra(SignUpActivity.EXTRA_Password)
+
+            AuthUsername = selectedEmail.toString()
+            Authpassword= selectedPass.toString()
+
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvClickSign = findViewById(R.id.tv_clicksignUp)
+        tvClickSign.setOnClickListener(this)
+
+        tvEmail = findViewById(R.id.edt_loginUser)
+        tvPassword = findViewById(R.id.edt_loginPass)
+        btnLogin = findViewById(R.id.btn_login)
+        btnLogin.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when ( v.id){
+            R.id.btn_login -> {
+
+                val Username = tvEmail.text.toString().trim()
+                val Password = tvPassword.text.toString().trim()
+                print(Password)
+
+                var empty = false
+                var auth = false
+
+                if(Username.isEmpty() || Username != AuthUsername ){
+                    empty = true
+                    auth = true
+                    tvEmail.error = "TextField Tidak Boleh kosong"
+                }
+                if(Password.isEmpty() ||  Password != Authpassword){
+                    empty = true
+                    auth = true
+                    tvPassword.error = "TextField Tidak Boleh kosong"
+                }
+
+                if (empty != true && auth != true){
+                    val mHomepage = Intent(this@MainActivity , HomePage::class.java)
+                    startActivity(mHomepage)
+                }
+            }
+            R.id.tv_clicksignUp -> {
+                val mSignupPage = Intent(this@MainActivity , SignUpActivity::class.java)
+                resultLauncher.launch(mSignupPage)
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/dhaveduvation/SignUpActivity.kt
@@ -0,0 +1,69 @@
+package com.example.dhaveduvation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tvName : TextView
+    private lateinit var tvUsername : TextView
+    private lateinit var tvEmail : TextView
+    private lateinit var tvPassword : TextView
+    private lateinit var tvComfirmpass: TextView
+    private lateinit var btnSignUp : Button
+
+    companion object {
+       const val EXTRA_Email = "EXTRA_EMAIL"
+       const val EXTRA_Password = "EXTRA_PASS"
+        const val RESULT_CODE = 110
+
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        tvName = findViewById(R.id.edt_RegisName)
+        tvUsername = findViewById(R.id.edt_RegisUsername)
+        tvEmail = findViewById(R.id.edt_RegisEmail)
+        tvPassword = findViewById(R.id.edt_RegisPassword)
+        tvComfirmpass = findViewById(R.id.edt_RegisConfirpass)
+        btnSignUp = findViewById(R.id.btn_Signup)
+
+        btnSignUp.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.btn_Signup -> {
+                val sUsername = tvUsername.text.toString().trim()
+                val sName = tvName.text.toString().trim()
+                val sEmail = tvEmail.text.toString().trim()
+                val sPassword = tvPassword.text.toString().trim()
+                val sConfirmPass = tvComfirmpass.text.toString().trim()
+
+                var auth = false
+
+                if (sPassword != sConfirmPass){
+                    auth = true
+                    tvComfirmpass.error = "Password not match"
+                }
+
+                if (auth != true){
+                    val sHomepage = Intent()
+                    sHomepage.putExtra(EXTRA_Email,sEmail)
+                    sHomepage.putExtra(EXTRA_Password,sPassword)
+                    setResult(RESULT_CODE, sHomepage)
+                    finish()
+                }
+            }
+
+
+        }
+
+    }
+}
